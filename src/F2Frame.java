c6bbce556ddca089af4f07627186ee6be57ed4f5
laurnew <lweiser@zagmail.gonzaga.edu>
2017-04-25 13:55:03 -0700

    can't restart hand until turn is over

--- src/F2Frame.java
+++ src/F2Frame.java
@@ -145,13 +145,13 @@ public class F2Frame extends JFrame {
 		JButton endGame = new JButton("End Game");
 
 		betAmount = JOptionPane.showInputDialog(null, "How much would you like to bet?");
-		String lineBet = JOptionPane.showInputDialog(null, "Which line (1-13) would you like to bet on?" +
-				 "\n 1, 2, 3, 4, 5, 6, 3 of a kind, 4 of a kind, full house, small straight, large straight, yahtzee");
+		String lineBet = JOptionPane.showInputDialog(null, "Which line (1-13) would you like to bet on?"
+				+ "\n 1, 2, 3, 4, 5, 6, 3 of a kind, 4 of a kind, full house, small straight, large straight, yahtzee");
 
 		bet = Integer.parseInt(betAmount); // gets int value of player's bet
 		line = Integer.parseInt(lineBet);
-		
-		while (line >13 || line < 1) {
+
+		while (line > 13 || line < 1) {
 			lineBet = JOptionPane.showInputDialog(null, "Please enter a number between 1 and 13:");
 			line = Integer.parseInt(lineBet);
 		}
@@ -192,99 +192,105 @@ public class F2Frame extends JFrame {
 		 **/
 		newHand.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				myHand.keepHand(keepThis);
-				keepThis = "nnnnn";
-				myHand.setCurrentHand(6);
-				ArrayList<Dice> hand = myHand.returnHand();
-				ImageIcon OG;
-				Dice t1 = hand.get(0);
-				int num1 = t1.getRoll();
-				if (num1 == 1) {
-					OG = icon;
-				} else if (num1 == 2) {
-					OG = icon2;
-				} else if (num1 == 3) {
-					OG = icon3;
-				} else if (num1 == 4) {
-					OG = icon4;
-				} else if (num1 == 5) {
-					OG = icon5;
+				int turns = myHand.getNumOfTurnsLeft();
+				if (turns != 2) {
+					JOptionPane.showMessageDialog(null,
+							"Error: you cannot restart hand until a current hand has ended");
 				} else {
-					OG = icon6;
+					myHand.keepHand(keepThis);
+					keepThis = "nnnnn";
+					myHand.setCurrentHand(6);
+					ArrayList<Dice> hand = myHand.returnHand();
+					ImageIcon OG;
+					Dice t1 = hand.get(0);
+					int num1 = t1.getRoll();
+					if (num1 == 1) {
+						OG = icon;
+					} else if (num1 == 2) {
+						OG = icon2;
+					} else if (num1 == 3) {
+						OG = icon3;
+					} else if (num1 == 4) {
+						OG = icon4;
+					} else if (num1 == 5) {
+						OG = icon5;
+					} else {
+						OG = icon6;
+					}
+					one.setIcon(OG);
+					Dice t2 = hand.get(1);
+					int num2 = t2.getRoll();
+					if (num2 == 1) {
+						OG = icon;
+					} else if (num2 == 2) {
+						OG = icon2;
+					} else if (num2 == 3) {
+						OG = icon3;
+					} else if (num2 == 4) {
+						OG = icon4;
+					} else if (num2 == 5) {
+						OG = icon5;
+					} else {
+						OG = icon6;
+					}
+					two.setIcon(OG);
+					Dice t3 = hand.get(2);
+					int num3 = t3.getRoll();
+					if (num3 == 1) {
+						OG = icon;
+					} else if (num3 == 2) {
+						OG = icon2;
+					} else if (num3 == 3) {
+						OG = icon3;
+					} else if (num3 == 4) {
+						OG = icon4;
+					} else if (num3 == 5) {
+						OG = icon5;
+					} else {
+						OG = icon6;
+					}
+					three.setIcon(OG);
+					Dice t4 = hand.get(3);
+					int num4 = t4.getRoll();
+					if (num4 == 1) {
+						OG = icon;
+					} else if (num4 == 2) {
+						OG = icon2;
+					} else if (num4 == 3) {
+						OG = icon3;
+					} else if (num4 == 4) {
+						OG = icon4;
+					} else if (num4 == 5) {
+						OG = icon5;
+					} else {
+						OG = icon6;
+					}
+					four.setIcon(OG);
+					Dice t5 = hand.get(4);
+					int num5 = t5.getRoll();
+					if (num5 == 1) {
+						OG = icon;
+					} else if (num5 == 2) {
+						OG = icon2;
+					} else if (num5 == 3) {
+						OG = icon3;
+					} else if (num5 == 4) {
+						OG = icon4;
+					} else if (num5 == 5) {
+						OG = icon5;
+					} else {
+						OG = icon6;
+					}
+					five.setIcon(OG);
 				}
-				one.setIcon(OG);
-				Dice t2 = hand.get(1);
-				int num2 = t2.getRoll();
-				if (num2 == 1) {
-					OG = icon;
-				} else if (num2 == 2) {
-					OG = icon2;
-				} else if (num2 == 3) {
-					OG = icon3;
-				} else if (num2 == 4) {
-					OG = icon4;
-				} else if (num2 == 5) {
-					OG = icon5;
-				} else {
-					OG = icon6;
-				}
-				two.setIcon(OG);
-				Dice t3 = hand.get(2);
-				int num3 = t3.getRoll();
-				if (num3 == 1) {
-					OG = icon;
-				} else if (num3 == 2) {
-					OG = icon2;
-				} else if (num3 == 3) {
-					OG = icon3;
-				} else if (num3 == 4) {
-					OG = icon4;
-				} else if (num3 == 5) {
-					OG = icon5;
-				} else {
-					OG = icon6;
-				}
-				three.setIcon(OG);
-				Dice t4 = hand.get(3);
-				int num4 = t4.getRoll();
-				if (num4 == 1) {
-					OG = icon;
-				} else if (num4 == 2) {
-					OG = icon2;
-				} else if (num4 == 3) {
-					OG = icon3;
-				} else if (num4 == 4) {
-					OG = icon4;
-				} else if (num4 == 5) {
-					OG = icon5;
-				} else {
-					OG = icon6;
-				}
-				four.setIcon(OG);
-				Dice t5 = hand.get(4);
-				int num5 = t5.getRoll();
-				if (num5 == 1) {
-					OG = icon;
-				} else if (num5 == 2) {
-					OG = icon2;
-				} else if (num5 == 3) {
-					OG = icon3;
-				} else if (num5 == 4) {
-					OG = icon4;
-				} else if (num5 == 5) {
-					OG = icon5;
-				} else {
-					OG = icon6;
-				}
-				five.setIcon(OG);
 			}
 		});
-		
-		/** the action listener for the end game button
-		 * is created
+
+		/**
+		 * the action listener for the end game button is created
 		 */
 		endGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event){
+			public void actionPerformed(ActionEvent event) {
 				System.exit(0);
 			}
 		});
@@ -318,20 +324,17 @@ public class F2Frame extends JFrame {
 					}
 					JOptionPane.showMessageDialog(null, "The hand has ended. You have $" + score.money);
 
-					String lineBet = JOptionPane.showInputDialog(null, "Which line (1-13) would you like to bet on?" +
-							 "\n 1, 2, 3, 4, 5, 6, 3 of a kind, 4 of a kind, full house, small straight, large straight, yahtzee");
+					String lineBet = JOptionPane.showInputDialog(null, "Which line (1-13) would you like to bet on?"
+							+ "\n 1, 2, 3, 4, 5, 6, 3 of a kind, 4 of a kind, full house, small straight, large straight, yahtzee");
 					line = Integer.parseInt(lineBet);
 					betAmount = JOptionPane.showInputDialog(null, "How much would you like to bet?");
 					bet = Integer.parseInt(betAmount);
-					while (line >13 || line < 1) {
+					while (line > 13 || line < 1) {
 						lineBet = JOptionPane.showInputDialog(null, "Please enter a number between 1 and 13:");
 						line = Integer.parseInt(lineBet);
 					}
 					flag = 0;
 				}
-			
-				
-				
 
 				myHand.keepHand(keepThis);
 				keepThis = "nnnnn";
